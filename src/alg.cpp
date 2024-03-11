7adeb02142ff40184adf045bd610ab28ef525c54
laal28 <129623228+laal28@users.noreply.github.com>
2024-03-11 10:59:25 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,14 +5,14 @@
 
 double pown(double value, uint16_t n) {
   double p = 1;
-  for( ; n > 0; --n )
+  for ( ; n > 0; --n )
     p *= value;
   return p;
 }
 
 uint64_t fact(uint16_t n) {
   uint64_t r = 1;
-  for( uint16_t i = 2; i <= n; i++ )
+  for ( uint16_t i = 2; i <= n; i++ )
     r *= i;
   return r;
 }
@@ -23,21 +23,21 @@ double calcItem(double x, uint16_t n) {
 
 double expn(double x, uint16_t count) {
   double exp = 2;
-  for( ; count > 1; count-- )
-    exp += calcItem( x, count );
+  for ( ; count > 1; count-- )
+    exp += calcItem(x, count);
   return exp;
 }
 
 double sinn(double x, uint16_t count) {
   double s = x;
-  for( ; count > 1; count-- )
-    s += pown( -1, count - 1 ) * calcItem( x, 2 * count - 1 );
+  for ( ; count > 1; count-- )
+    s += pown(-1, count - 1) * calcItem(x, 2 * count - 1);
   return s;
 }
 
 double cosn(double x, uint16_t count) {
   double s = 1;
-  for( ; count > 1; count-- )
-    s += pown( -1, count - 1 ) * calcItem( x, 2 * count - 2 );
+  for ( ; count > 1; count-- )
+    s += pown(-1, count - 1) * calcItem(x, 2 * count - 2);
   return s;
 }
